2d7c69df2e9c463d420e7054ea4fd607872cad28
DavidHE39 <73972156+DavidHE39@users.noreply.github.com>
2025-05-06 10:04:38 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold mb-4">Welcome to My Business</h2>
+      <p>We provide Microsoft 365, Power Platform, and secure cloud solutions.</p>
+    </section>
+  )
+}
