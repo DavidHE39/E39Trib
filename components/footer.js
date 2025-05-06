7ec98ea11c93339d50ed990310ee10489380b24a
DavidHE39 <73972156+DavidHE39@users.noreply.github.com>
2025-05-06 10:09:54 -0400

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 text-center text-sm border-t dark:border-gray-700">
+      © {new Date().getFullYear()} My Business. All rights reserved.
+    </footer>
+  )
+}
